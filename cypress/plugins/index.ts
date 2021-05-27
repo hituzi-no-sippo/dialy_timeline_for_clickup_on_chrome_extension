65af7d17815794ea09cda6af94f7648dc581d2c3
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-05-27 20:52:30 +0900

    test(cypress): add test watch

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,8 @@
+import 'cypress-watch-and-reload/plugins'
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// The `pluginsFile` must export a function
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+module.exports = () => {}
