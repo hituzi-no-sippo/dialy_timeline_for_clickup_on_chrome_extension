6f5707126c629a3b3917f4e5a669bce0be0189cd
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-05-31 16:23:32 +0900

    test(cypress): set up component test

---
+++ cypress/component/operation_check.ts
@@ -0,0 +1,8 @@
+import { mount } from '@cypress/vue'
+import App from '@/App.vue'
+
+describe('operation check of test', () => {
+  it('component test', () => {
+    mount(App)
+  })
+})
--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,12 +1,21 @@
 import 'cypress-watch-and-reload/plugins'
 import { startDevServer } from '@cypress/vite-dev-server'
+import path from 'path'
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   on('dev-server:start', async (options) => {
-    return startDevServer({ options })
+    const viteConfig = {
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../../src'),
+        }
+      },
+    }
+
+    return startDevServer({ options, viteConfig })
   })
 
   return config
